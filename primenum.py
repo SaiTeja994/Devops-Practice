e707d28151fb7cd48b67e88f9d8192eabd8cbf21
SaiTeja994 <saitejabittu2001@gmail.com>
2025-08-06 20:18:31 +0530

    Describe what you changed

--- primenum.py
+++ primenum.py
@@ -1,14 +1,10 @@
-#python code for prime number
-def is_prime(n):
-    if n <= 1:
-        return False
-    if n == 2:
-        return True
-    if n % 2 == 0:
-        return False
-    max_divisor = int(n**0.5) + 1
-    for d in range(3, max_divisor, 2):
-        if n % d == 0:
-            return False
-        return True
-    
+#write a python code for prime numbers between 1 t0 100
+for i in range(1,101):
+    flag = 0
+    for j in range(2, i):
+        if(i % j == 0):
+            flag = 1
+            break
+        if(flag == 0):
+            print(i)
+            
